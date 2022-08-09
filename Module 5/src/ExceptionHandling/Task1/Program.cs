ea6c4d2dafb1f551cc09aecdaf0249a8bd8d5386
dasAtRagedy <taitragedija@gmail.com>
2022-08-09 14:26:32 +0300

    Method Main now prints the first character of each entered input line

--- Module 5/src/ExceptionHandling/Task1/Program.cs
+++ Module 5/src/ExceptionHandling/Task1/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            var input = Console.ReadLine();
+            try
+            {
+                if (string.IsNullOrEmpty(input))
+                    throw new ArgumentNullException(input, "Input is empty!");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }
