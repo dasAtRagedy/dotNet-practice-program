5d7548518dc8011079f9c631d5bac24a7d54941a
dasAtRagedy <taitragedija@gmail.com>
2022-08-09 14:27:43 +0300

    string to int type parser without the usage of in-built c# methods

--- Module 5/src/ExceptionHandling/Task2/NumberParser.cs
+++ Module 5/src/ExceptionHandling/Task2/NumberParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Task2
 {
@@ -6,7 +7,36 @@ namespace Task2
     {
         public int Parse(string stringValue)
         {
-            throw new NotImplementedException();
+            if (stringValue == null)
+                throw new ArgumentNullException();
+            if (string.IsNullOrWhiteSpace(stringValue))
+                throw new FormatException();
+            
+            stringValue = Regex.Replace(stringValue, @"\s+", "");
+            
+            int result = 0;
+            int power = 1;
+            for (int i = stringValue.Length - 1; i >= 0; i--)
+            {
+                if ((stringValue[i] == '+' || stringValue[i] == '-') && i == 0)
+                    continue;
+                if (char.IsDigit(stringValue[i]))
+                {
+                    checked
+                    {
+                        if(stringValue[0] == '-')
+                            result -= power * (stringValue[i] - '0');
+                        else
+                            result += power * (stringValue[i] - '0');
+                    }
+                    power *= 10;
+                }
+                else
+                {
+                    throw new FormatException("String contains non-digit characters");
+                }
+            }
+            return result;
         }
     }
 }
