aae789d91c9e760ec2f515f85a44239414bfbd9e
dasAtRagedy <taitragedija@gmail.com>
2022-08-23 23:35:01 +0300

    Test 1

--- Module 8/TDDApps/OddEven/OddEven.cs
+++ Module 8/TDDApps/OddEven/OddEven.cs
@@ -2,5 +2,13 @@
 
 public class OddEvenClass
 {
-    
+    public void PrintNumbers()
+    {
+        for(int i = 1; i <= 100; i++)
+            Console.WriteLine(Parse(i));
+    }
+    public static int Parse(int num)
+    {
+        return num;
+    }
 }
--- Module 8/TDDApps/OddEven/OddEven_Tests.cs
+++ Module 8/TDDApps/OddEven/OddEven_Tests.cs
@@ -2,9 +2,15 @@ namespace OddEven;
 
 public class OddEvenTests
 {
-    [Fact]
-    public void OddEven_PrintNumbersFromOneToHundred()
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(5, 5)]
+    [InlineData(27, 27)]
+    [InlineData(59, 59)]
+    [InlineData(100, 100)]
+    public void ParseNumberTest(int number, int expectedResult)
     {
-
+        Assert.Equal(expectedResult, OddEvenClass.Parse(number));
     }
 }
