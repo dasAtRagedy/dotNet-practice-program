320aa1ca6b5b242e2c5616132a31e31825989e66
dasAtRagedy <taitragedija@gmail.com>
2022-08-23 23:54:43 +0300

    Test 4

--- Module 8/TDDApps/OddEven/OddEven.cs
+++ Module 8/TDDApps/OddEven/OddEven.cs
@@ -9,8 +9,18 @@ public class OddEvenClass
     }
     public static string Parse(int num)
     {
-        if (num % 2 == 0)
-            return "Even";
-        return "Odd";
+        if (IsPrime(num))
+            return num.ToString();
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+    public static bool IsPrime(int num)
+    {
+        if (num == 1)
+            return false;
+        for (int i = 2; i < num; i++)
+            if (num % i == 0)
+                return false;
+        return true;
     }
 }
--- Module 8/TDDApps/OddEven/OddEven_Tests.cs
+++ Module 8/TDDApps/OddEven/OddEven_Tests.cs
@@ -4,17 +4,34 @@ public class OddEvenTests
 {
     [Theory]
     [InlineData(1, "Odd")]
-    [InlineData(2, "Even")]
-    [InlineData(5, "Odd")]
-    [InlineData(6, "Even")]
-    [InlineData(27, "Odd")]
-    [InlineData(28, "Even")]
-    [InlineData(54, "Even")]
-    [InlineData(59, "Odd")]
-    [InlineData(99, "Odd")]
-    [InlineData(100, "Even")]
+    [InlineData(2, "2")]
+    [InlineData(13, "13")]
+    [InlineData(24, "Even")]
+    [InlineData(35, "Odd")]
+    [InlineData(46, "Even")]
+    [InlineData(57, "Odd")]
+    [InlineData(68, "Even")]
+    [InlineData(79, "79")]
+    [InlineData(90, "Even")]
     public void ParseNumberTest(int number, string expectedResult)
     {
         Assert.Equal(expectedResult, OddEvenClass.Parse(number));
     }
+    
+    [Theory]
+    [InlineData(1, false)]
+    [InlineData(2, true)]
+    [InlineData(13, true)]
+    [InlineData(24, false)]
+    [InlineData(35, false)]
+    [InlineData(46, false)]
+    [InlineData(57, false)]
+    [InlineData(68, false)]
+    [InlineData(79, true)]
+    [InlineData(90, false)]
+    public void IsPrimeTest(int number, bool expectedResult)
+    {
+        Assert.Equal(expectedResult, OddEvenClass.IsPrime(number));
+    }
+    
 }
